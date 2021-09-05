806c8aa8a08658744d4f6538f2d43f6091725b76
Moaaz Mohammed <84313242+Moaaz-Mohammed@users.noreply.github.com>
2021-09-05 18:41:58 +0200

    .

--- lib/Screens/AddPatient.dart
+++ lib/Screens/AddPatient.dart
@@ -27,7 +27,11 @@ class AddPatientScreen extends StatelessWidget {
         ),
         centerTitle: true,
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        leading: InkWell(onTap:(){Navigator.pop(context);},child: Icon(Icons.arrow_back_ios, color: Colors.black)),
+        leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(Icons.arrow_back_ios, color: Colors.black)),
       ),
       body: Padding(
         padding: EdgeInsets.all(20),
@@ -37,12 +41,12 @@ class AddPatientScreen extends StatelessWidget {
             child: Column(
               children: [
                 TextFormField(
-                  validator: (value){
-                        if (value!.isEmpty) {
-                          return 'Required';
-                        }
-                        return null;
-                      },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Required';
+                    }
+                    return null;
+                  },
                   controller: nameController,
                   decoration: InputDecoration(
                     hintText: 'Name',
@@ -51,54 +55,53 @@ class AddPatientScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(height:height*0.01),
-                Row(
-                  children:[
-                    Expanded(
-                      child: TextFormField(
-                        validator: (value)
-                        {
-                          if(value!.isEmpty)
-                            return 'Required Field';
-                        },
-                        controller: ageController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          hintText: 'Age',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: width * 0.02),
-                    Expanded(
-                      child: TextFormField(
-                        validator: (value)
-                        {
-                          if(value!.isEmpty)
-                            return 'Required Field';
-                        },
-                        controller: phoneController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          hintText: 'Phone',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ]
-                ),
-                SizedBox(height:height*0.01),
-                TextFormField(
-                  validator: (value){
+                SizedBox(height: height * 0.01),
+                Row(children: [
+                  Expanded(
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
                         if (value!.isEmpty) {
                           return 'Required';
                         }
                         return null;
                       },
+                      controller: ageController,
+                      decoration: InputDecoration(
+                        hintText: 'Age',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: width * 0.02),
+                  Expanded(
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Required';
+                        }
+                        return null;
+                      },
+                      controller: phoneController,
+                      decoration: InputDecoration(
+                        hintText: 'Phone',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ]),
+                SizedBox(height: height * 0.01),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Required';
+                    }
+                    return null;
+                  },
                   controller: addressController,
                   decoration: InputDecoration(
                     hintText: 'Address',
@@ -107,15 +110,15 @@ class AddPatientScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(height:height*0.01),
+                SizedBox(height: height * 0.01),
                 TextFormField(
-                  validator: (value){
-                        if (value!.isEmpty) {
-                          return 'Required';
-                        }
-                        return null;
-                      },
-                  controller:diagnosesController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Required';
+                    }
+                    return null;
+                  },
+                  controller: diagnosesController,
                   minLines: 3,
                   maxLines: 10,
                   decoration: InputDecoration(
@@ -125,14 +128,14 @@ class AddPatientScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(height:height*0.01),
+                SizedBox(height: height * 0.01),
                 TextFormField(
-                  validator: (value){
-                        if (value!.isEmpty) {
-                          return 'Required';
-                        }
-                        return null;
-                      },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Required';
+                    }
+                    return null;
+                  },
                   controller: treatmentController,
                   minLines: 3,
                   maxLines: 10,
@@ -143,15 +146,8 @@ class AddPatientScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(height:height*0.01),
+                SizedBox(height: height * 0.01),
                 TextFormField(
-                  validator: (value){
-                    if (value!.isEmpty)
-                    {
-                      return 'Required';
-                    }
-                    return null;
-                  },
                   controller: noteController,
                   minLines: 3,
                   maxLines: 10,
@@ -162,43 +158,60 @@ class AddPatientScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(height: height * 0.02,),
+                SizedBox(
+                  height: height * 0.02,
+                ),
                 InkWell(
-                  onTap: ()
-                  {
+                  onTap: () {
                     if (formKey.currentState!.validate()) {
-                      final firestoreInstance =
-                          FirebaseFirestore.instance;
-                      firestoreInstance.collection("Patients").add({
-                        "name": nameController.text,
-                        "age": ageController.text,
-                        "phone": phoneController.text,
-                        "address": addressController.text,
-                        "diagnoses": diagnosesController.text,
-                        "treatment": treatmentController.text,
-                        "time": DateTime.now().toLocal(),
-                      }).then((value)=> showDialog(context: context, builder: (context)=>CustomDialog(
-                        title: 'Data Saved Successfully!',
-                      ))).then((value) =>  Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen())));
+                      final firestoreInstance = FirebaseFirestore.instance;
+                      firestoreInstance
+                          .collection("Patients")
+                          .add({
+                            "name": nameController.text,
+                            "age": ageController.text,
+                            "phone": phoneController.text,
+                            "address": addressController.text,
+                            "diagnoses": diagnosesController.text,
+                            "treatment": treatmentController.text,
+                            "time": DateTime.now().toLocal(),
+                          })
+                          .then((value) => showDialog(
+                              context: context,
+                              builder: (context) => CustomDialog(
+                                    title: 'Data Saved Successfully!',
+                                  )))
+                          .then((value) => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomeScreen())));
                     }
-
                   },
-
                   child: Container(
-                    child: Center(child: Padding(
+                    child: Center(
+                        child: Padding(
                       padding: const EdgeInsets.all(20.0),
-                      child: Text('SAVE',style: TextStyle(shadows:[BoxShadow(
-                        color: Colors.white,
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 3), // changes position of shadow
-                      ),],fontSize: 24,color:Colors.white,fontWeight: FontWeight.bold),),
+                      child: Text(
+                        'SAVE',
+                        style: TextStyle(
+                            shadows: [
+                              BoxShadow(
+                                color: Colors.white,
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset:
+                                    Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
                     )),
                     decoration: BoxDecoration(
-                      color:Colors.black,
-                      borderRadius: BorderRadius.circular(20),
-                      border:Border.all(color:Colors.white,width: 3)
-                    ),
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(color: Colors.white, width: 3)),
                   ),
                 )
               ],

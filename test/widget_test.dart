6610f95e4b67c51d59545172b7c2d6997cfbc373
Moaaz Mohammed <84313242+Moaaz-Mohammed@users.noreply.github.com>
2021-09-05 11:24:10 +0200

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:clinic_mob/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Clinic Application', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 

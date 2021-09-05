da647cc628af0780d94af3de5819fe855d8f3544
Moaaz Mohammed <84313242+Moaaz-Mohammed@users.noreply.github.com>
2021-09-05 14:28:21 +0200

    .

--- lib/Screens/AddPatient.dart
+++ lib/Screens/AddPatient.dart
@@ -6,8 +6,7 @@ import 'package:flutter/material.dart';
 import 'HomeScreen.dart';
 
 class AddPatientScreen extends StatelessWidget {
-  const AddPatientScreen({Key? key}) : super(key: key);
-
+  var formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     TextEditingController nameController = TextEditingController();
@@ -18,7 +17,6 @@ class AddPatientScreen extends StatelessWidget {
     TextEditingController phoneController = TextEditingController();
     TextEditingController noteController = TextEditingController();
 
-    var formKey = GlobalKey<FormState>();
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
@@ -34,173 +32,177 @@ class AddPatientScreen extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(20),
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              TextFormField(
-                validator: (value){
-                      if (value!.isEmpty) {
-                        return 'Required';
-                      }
-                      return null;
-                    },
-                controller: nameController,
-                decoration: InputDecoration(
-                  hintText: 'Name',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  validator: (value){
+                        if (value!.isEmpty) {
+                          return 'Required';
+                        }
+                        return null;
+                      },
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    hintText: 'Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height:height*0.01),
-              Row(
-                children:[
-                  Expanded(
-                    child: TextFormField(
-                      validator: (value)
-                      {
-                        if(value!.isEmpty)
-                          return 'Required Field';
-                      },
-                      controller: ageController,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        hintText: 'Age',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+                SizedBox(height:height*0.01),
+                Row(
+                  children:[
+                    Expanded(
+                      child: TextFormField(
+                        validator: (value)
+                        {
+                          if(value!.isEmpty)
+                            return 'Required Field';
+                        },
+                        controller: ageController,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          hintText: 'Age',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(width: width * 0.02),
-                  Expanded(
-                    child: TextFormField(
-                      validator: (value)
-                      {
-                        if(value!.isEmpty)
-                          return 'Required Field';
-                      },
-                      controller: phoneController,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        hintText: 'Phone',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+                    SizedBox(width: width * 0.02),
+                    Expanded(
+                      child: TextFormField(
+                        validator: (value)
+                        {
+                          if(value!.isEmpty)
+                            return 'Required Field';
+                        },
+                        controller: phoneController,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          hintText: 'Phone',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ]
-              ),
-              SizedBox(height:height*0.01),
-              TextFormField(
-                validator: (value){
-                      if (value!.isEmpty) {
-                        return 'Required';
-                      }
-                      return null;
-                    },
-                controller: addressController,
-                decoration: InputDecoration(
-                  hintText: 'Address',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                  ]
+                ),
+                SizedBox(height:height*0.01),
+                TextFormField(
+                  validator: (value){
+                        if (value!.isEmpty) {
+                          return 'Required';
+                        }
+                        return null;
+                      },
+                  controller: addressController,
+                  decoration: InputDecoration(
+                    hintText: 'Address',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height:height*0.01),
-              TextFormField(
-                validator: (value){
-                      if (value!.isEmpty) {
-                        return 'Required';
-                      }
-                      return null;
-                    },
-                controller:diagnosesController,
-                minLines: 3,
-                maxLines: 10,
-                decoration: InputDecoration(
-                  hintText: 'Diagnoses',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                SizedBox(height:height*0.01),
+                TextFormField(
+                  validator: (value){
+                        if (value!.isEmpty) {
+                          return 'Required';
+                        }
+                        return null;
+                      },
+                  controller:diagnosesController,
+                  minLines: 3,
+                  maxLines: 10,
+                  decoration: InputDecoration(
+                    hintText: 'Diagnoses',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height:height*0.01),
-              TextFormField(
-                validator: (value){
-                      if (value!.isEmpty) {
-                        return 'Required';
-                      }
-                      return null;
-                    },
-                controller: treatmentController,
-                minLines: 3,
-                maxLines: 10,
-                decoration: InputDecoration(
-                  hintText: 'Treatment',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                SizedBox(height:height*0.01),
+                TextFormField(
+                  validator: (value){
+                        if (value!.isEmpty) {
+                          return 'Required';
+                        }
+                        return null;
+                      },
+                  controller: treatmentController,
+                  minLines: 3,
+                  maxLines: 10,
+                  decoration: InputDecoration(
+                    hintText: 'Treatment',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height:height*0.01),
-              TextFormField(
-                validator: (value){
-                  if (value!.isEmpty) {
-                    return 'Required';
-                  }
-                  return null;
-                },
-                controller: noteController,
-                minLines: 3,
-                maxLines: 10,
-                decoration: InputDecoration(
-                  hintText: 'Notes',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                SizedBox(height:height*0.01),
+                TextFormField(
+                  validator: (value){
+                    if (value!.isEmpty)
+                    {
+                      return 'Required';
+                    }
+                    return null;
+                  },
+                  controller: noteController,
+                  minLines: 3,
+                  maxLines: 10,
+                  decoration: InputDecoration(
+                    hintText: 'Notes',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: height * 0.02,),
-              InkWell(
-                onTap: ()
-                {
-                  if (formKey.currentState!.validate()) {
-                    final firestoreInstance =
-                        FirebaseFirestore.instance;
-                    firestoreInstance.collection("Patients").add({
-                      "name": nameController.text,
-                      "age": ageController.text,
-                      "phone": phoneController.text,
-                      "address": addressController.text,
-                      "diagnoses": diagnosesController.text,
-                      "treatment": treatmentController.text,
-                      "time": DateTime.now().toLocal(),
-                    }).then((value)=> showDialog(context: context, builder: (context)=>CustomDialog(
-                      title: 'Data Saved Successfully!',
-                    ))).then((value) =>  Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen())));
-                  }
+                SizedBox(height: height * 0.02,),
+                InkWell(
+                  onTap: ()
+                  {
+                    if (formKey.currentState!.validate()) {
+                      final firestoreInstance =
+                          FirebaseFirestore.instance;
+                      firestoreInstance.collection("Patients").add({
+                        "name": nameController.text,
+                        "age": ageController.text,
+                        "phone": phoneController.text,
+                        "address": addressController.text,
+                        "diagnoses": diagnosesController.text,
+                        "treatment": treatmentController.text,
+                        "time": DateTime.now().toLocal(),
+                      }).then((value)=> showDialog(context: context, builder: (context)=>CustomDialog(
+                        title: 'Data Saved Successfully!',
+                      ))).then((value) =>  Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen())));
+                    }
 
-                },
+                  },
 
-                child: Container(
-                  child: Center(child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text('SAVE',style: TextStyle(shadows:[BoxShadow(
-                      color: Colors.white,
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),],fontSize: 24,color:Colors.white,fontWeight: FontWeight.bold),),
-                  )),
-                  decoration: BoxDecoration(
-                    color:Colors.black,
-                    borderRadius: BorderRadius.circular(20),
-                    border:Border.all(color:Colors.white,width: 3)
+                  child: Container(
+                    child: Center(child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text('SAVE',style: TextStyle(shadows:[BoxShadow(
+                        color: Colors.white,
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),],fontSize: 24,color:Colors.white,fontWeight: FontWeight.bold),),
+                    )),
+                    decoration: BoxDecoration(
+                      color:Colors.black,
+                      borderRadius: BorderRadius.circular(20),
+                      border:Border.all(color:Colors.white,width: 3)
+                    ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),

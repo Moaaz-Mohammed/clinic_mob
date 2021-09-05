57e998efb59ff0161623ae940e589e062398fccc
Moaaz Mohammed <84313242+Moaaz-Mohammed@users.noreply.github.com>
2021-09-05 11:28:13 +0200

    .

--- lib/Widgets/Drawer/CustomDrawerHeader.dart
+++ lib/Widgets/Drawer/CustomDrawerHeader.dart
@@ -9,15 +9,18 @@ class CustomDrawerHeader extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     return Container(
       margin: EdgeInsets.only(top: 10,bottom: 10),
-      child: Column(
+      child: Column
+        (
         children:
         [
           CircleAvatar
             (
             radius: height * 0.06,
+            backgroundImage: NetworkImage('https://styles.redditmedia.com/t5_31m8g/styles/communityIcon_ytz903nfwl211.png'),
+            backgroundColor: Colors.transparent,
             ),
           SizedBox(height:height * 0.015),
-          Text('Name Name')
+          Text('Name Name',style: TextStyle(fontSize:20,fontWeight:FontWeight.bold),)
         ],
       ),
     );
